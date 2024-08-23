c810df7942978c53cbd891bf0c3d774cb94e0db8
dalvarez <andresdaap2015@gmail.com>
2024-08-23 11:56:51 -0400

    Add logger and logs folder

---
+++ shared/libs/logger.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+
+def setup_logger_level(logger_name: str, filename: str | None = None):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setFormatter(formatter)
+
+    logger.addHandler(sh)
+
+    if filename:
+        fh = logging.FileHandler(f"./logs/{filename}.log")
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    return logger
